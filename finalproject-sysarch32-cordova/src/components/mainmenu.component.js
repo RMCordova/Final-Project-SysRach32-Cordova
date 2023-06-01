fe41a7c6f7b242111f2e2cac165926958548f60b
RMCordova <128437412+RMCordova@users.noreply.github.com>
2023-06-01 11:38:01 +0800

    3rd commit

--- finalproject-sysarch32-cordova/src/components/mainmenu.component.js
+++ finalproject-sysarch32-cordova/src/components/mainmenu.component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './MainMenu.css'; // Import the CSS file for styling
+import './mainmenu.component.css';
 
 function MainMenu() {
   return (
