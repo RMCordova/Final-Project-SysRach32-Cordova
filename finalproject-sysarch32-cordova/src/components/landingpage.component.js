21a137f43295d7ae9406ff1dfd22beacf612b0af
RMCordova <128437412+RMCordova@users.noreply.github.com>
2023-06-04 10:29:36 +0800

    5th commit

--- finalproject-sysarch32-cordova/src/components/landingpage.component.js
+++ finalproject-sysarch32-cordova/src/components/landingpage.component.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import './landingpage.component.css';
+import MainMenuPage from './mainmenu.component';
 
 function LandingPage() {
   return (
     <div className="landing-page-container">
       <h1>Welcome to the Landing Page</h1>
       <p>This is the landing page content. Start exploring now!</p>
-      <Link to="/MainMenu" className="btn btn-primary">Explore</Link>
+      <Link to="/mainmenu-page" className="btn btn-primary">Explore</Link>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route path="/mainmenu-page" element={<MainMenuPage />} />
+      </Routes>
     </div>
   );
 }
 
 export default LandingPage;
+
