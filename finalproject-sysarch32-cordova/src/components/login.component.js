d1d4ad22421eb80e5800caf69dd7acb77efc6f56
RMCordova <128437412+RMCordova@users.noreply.github.com>
2023-06-07 04:19:19 +0800

    13th commit

--- finalproject-sysarch32-cordova/src/components/login.component.js
+++ finalproject-sysarch32-cordova/src/components/login.component.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
-export default class Login extends Component {
-  handleSubmit = async (event) => {
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
       const response = await axios.post('/sign-in', {
-        email: formData.get('email'),
-        password: formData.get('password')
+        email,
+        password
       });
 
       console.log(response.data);
       // Handle success (e.g., redirect to a dashboard page)
       alert('Logged in successfully!');
-      
+
       // Optionally, you can reset the form fields
-      form.reset();
+      setEmail('');
+      setPassword('');
     } catch (error) {
       console.error('Error logging in:', error);
       // Handle error (e.g., show an error message)
@@ -24,48 +28,53 @@ export default class Login extends Component {
     }
   };
 
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <h3>Sign In</h3>
-        <div className="mb-3">
-          <label>Email address</label>
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Sign In</h3>
+      <div className="mb-3">
+        <label>Email address</label>
+        <input
+          type="email"
+          name="email"
+          className="form-control"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="mb-3">
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          className="form-control"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="mb-3">
+        <div className="custom-control custom-checkbox">
           <input
-            type="email"
-            name="email"
-            className="form-control"
-            placeholder="Enter email"
+            type="checkbox"
+            className="custom-control-input"
+            id="customCheck1"
           />
+          <label className="custom-control-label" htmlFor="customCheck1">
+            Remember me
+          </label>
         </div>
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            className="form-control"
-            placeholder="Enter password"
-          />
-        </div>
-        <div className="mb-3">
-          <div className="custom-control custom-checkbox">
-            <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
-            />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Remember me
-            </label>
-          </div>
-        </div>
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </div>
-        <p className="forgot-password text-right">
-          Forgot <a href="#">password?</a>
-        </p>
-      </form>
-    );
-  }
-}
+      </div>
+      <div className="d-grid">
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </div>
+      <p className="forgot-password text-right">
+        Forgot <a href="#">password?</a>
+      </p>
+    </form>
+  );
+};
+
+export default Login;
